300e34d904bc670ced689d4e3b0a1a9b36e37619
Banzi Rahim <rahimbanzii@gmail.com>
2023-03-16 16:52:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a line usimg the printf function
+ * Return: o (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
