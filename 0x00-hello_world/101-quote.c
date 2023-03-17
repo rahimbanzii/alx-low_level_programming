a92afd7beb6e7857c445da1bde14652fc010a9b2
Banzi Rahim <rahimbanzii@gmail.com>
2023-03-16 18:52:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program that prints a line to the standard error
+ * main - a program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
