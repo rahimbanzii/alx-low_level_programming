b2c56de2aa9d353b55423fbd27964d2215377aa4
Banzi Rahim <rahimbanzii@gmail.com>
2023-04-06 06:03:35 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints string in reverse
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
